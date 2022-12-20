d3fc661cdb228536375ff26c3fd011c7d6925a07
Sanzhar <sanzharbekbolat1@gmail.com>
2022-12-20 22:38:10 +0600

    rating completed

--- kaspiShop/Pages/ProductDetail.swift
+++ kaspiShop/Pages/ProductDetail.swift
@@ -11,45 +11,61 @@ struct ProductDetail: View {
     var product : Product
     @State var review: String = ""
     @State var rating: Int = 0
+    @State var showingAlert = false
+    @State var readyForSend = false
     
     @StateObject var favorites = Favorites()
     
         var body: some View {
+            
                     NavigationView {
-                        VStack{
-                            HStack{
-                                Image("electronics")
-                                    .resizable()
-                                    .frame(width: 350, height: 350)
-                                    .padding()
-
-                            }
-                            
-                            Button(favorites.contains(product) ?"Remove from wish list": "Add to wish list",
-                                   action: {
-                                        if favorites.contains(product) {
-                                                favorites.remove(product)
-                                        }
-                                        else {
-                                            favorites.add(product)
-                                        }
-                            }).buttonStyle(.borderedProminent)
-                            .padding()
-                            
-                            Text("brand: "+product.image)
-                            Text("description: "+product.name)
-                            Text("price: "+"\(product.price)$")
-                            Text("author: " + "\(product.id)#")
-                            // Rating for product
-                            VStack{
-                                Section {
-                                    TextEditor(text: $review)
-                                    RatingView(rating: rating)
-                                } header: {
-                                    Text("Write a review")
+                        ScrollView{
+                            VStack {
+                                HStack{
+                                    Image("electronics")
+                                        .resizable()
+                                        .frame(width: 350, height: 350)
+                                        .padding()
+                                    
                                 }
+                                
+                                Button(favorites.contains(product) ?"Remove from wish list": "Add to wish list",
+                                       action: {
+                                    if favorites.contains(product) {
+                                        favorites.remove(product)
+                                    }
+                                    else {
+                                        favorites.add(product)
+                                    }
+                                }).buttonStyle(.borderedProminent)
+                                    .padding()
+                                
+                                Text("brand: "+product.image)
+                                Text("description: "+product.name)
+                                Text("price: "+"\(product.price)$")
+                                Text("author: " + "\(product.id)#")
+                                // Rating for product
+                                VStack{
+                                    Section {
+                                        TextField("Enter your comment ", text: $review)
+                                            .padding()
+                                        
+                                        RatingView(rating: rating)
+                                    } header: {
+                                        Text("Write a review")
+                                    }
+                                    Button("Send review" ) {
+                                        showingAlert = true
+                                    }
+                                    .padding()
+                                    .background(Color.blue)
+                                    .disabled(review.count == 0)
+                                    .alert("Your review sent", isPresented: $showingAlert){
+                                        Button("OK", role: .cancel) {}
+                                    }
+                                }
+                                
                             }
-                            
                         }
 
                     }.navigationBarTitle(product.name)
---
+++ kaspiShop/Pages/ReviewSend.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewSend.swift
+//  kaspiShop
+//
+//  Created by Sanzhar Bekbolat on 20.12.2022.
+//
+
+import SwiftUI
+
+struct ReviewSend: View {
+    var body: some View {
+        Text("You review ")
+    }
+}
+
+struct ReviewSend_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewSend()
+    }
+}
